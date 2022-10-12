82bb4f33e8f4ae51d144e9399212493c4934c428
OwenSal16 <owensalciccioli@gmail.com>
2022-10-11 23:59:35 -0400

    Doing way less work than I should have

--- RetentionTestCode.R
+++ RetentionTestCode.R
@@ -4,4 +4,14 @@ year <- c(2016, 2017, 2020, 2021, 2022, 2011, 2012, 2013, 2015, 2015, 2020, 2022
 
 test_data <- data.frame(entity, year)
 
-testing
+retained <- c()
+i <- 1
+lastYear <- NA
+highestvalue <- 0
+for(i in 1:length(test_data$year)) {
+  entityID <- test_data$entity[i]
+  years <- c()
+  # we need a function (probably a loop) that takes in the entity ID, looks for every iteration of that ID, and make a vector of the year results. Then we can do the looping
+  i <- i + 1
+}
+  
